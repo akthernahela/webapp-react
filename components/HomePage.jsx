3aaf4b7d6387983c94d13cf354c7d03fbf7b363e
akthernahela <nahela00@gmail.com>
2025-12-11 20:26:45 +0100

    Import bootstrap card

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -2,7 +2,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function HomePage() {
-    return
+
+    const urlMovies = 'http://localhost:3000/';
+
+    return (
+        <>
+            <div className="container">
+                <div class="card mb-3">
+
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default HomePage;
