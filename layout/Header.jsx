08ac55513bd37ce9dab88b106e533fc2d2593b15
akthernahela <nahela00@gmail.com>
2025-12-11 18:31:20 +0100

    Fix the navbar

--- layout/Header.jsx
+++ layout/Header.jsx
@@ -6,20 +6,17 @@ export default function Header() {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <NavLink className="navbar-brand" to="/">Navbar</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <Link className="nav-link" aria-current="page" to="/">HomePage</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <Link className="nav-link" aria-current="page" to="/movie/:id">Movie</Link>
                             </li>
                         </ul>
                         <form className="d-flex" role="search">
