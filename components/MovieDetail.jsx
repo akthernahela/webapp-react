229c6faec4742844f2629c719cad440fa09795bf
akthernahela <nahela00@gmail.com>
2025-12-15 19:41:37 +0100

    Fix the error

--- components/MovieDetail.jsx
+++ components/MovieDetail.jsx
@@ -5,27 +5,28 @@ import axios from "axios";
 function MovieDetail() {
 
     const { id } = useParams();
-    const [movies, setMovies] = useState([]);
-    const urlMovies = 'http://localhost:3000/api/movies/';
+    const [movie, setMovies] = useState([]);
+
     useEffect(() => {
-        axios.get(urlMovies).then(res => {
-            console.log(res.data);
-            setMovies(res.data);
-        });
+        axios.get(`http://localhost:3000/api/movie/${id}`)
+            .then(res => {
+                console.log(res.data);
+                setMovies(res.data);
+            });
     }, [id]);
 
     return (
         <div className="container">
-            <div className="card m-3" key={movies.id}>
-                <img src={`http://localhost:3000/images/${movies.image}`} alt="" className="" />
+            <div className="card m-3" key={movie.id}>
+                <img src={`http://localhost:3000/images/${movie.image}`} alt="" className="" />
                 <div className="card-body">
-                    <h5 className="card-title">{movies.title}</h5>
-                    <p className="card-text">{movies.director}</p>
-                    <p className="card-text">{movies.genre}</p>
+                    <h5 className="card-title">{movie.title}</h5>
+                    <p className="card-text">{movie.director}</p>
+                    <p className="card-text">{movie.genre}</p>
                     <p className="card-text">
-                        <small className="text-body-secondary">{movies.release_year}</small>
+                        <small className="text-body-secondary">{movie.release_year}</small>
                     </p>
-                    <Link className="btn btn-dark mt-auto" to={`/movies/${movies.id}`}>
+                    <Link className="btn btn-dark mt-auto" to={`/movie/${movie.id}`}>
                         Recensione
                     </Link>
                 </div>
