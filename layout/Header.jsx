640f1d18274e755e5d081344a2a63fbb07a3e9be
akthernahela <nahela00@gmail.com>
2025-12-15 23:07:52 +0100

    Edit the button color

--- layout/Header.jsx
+++ layout/Header.jsx
@@ -21,7 +21,7 @@ export default function Header() {
                         </ul>
                         <form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
+                            <button className="btn btn-dark" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
