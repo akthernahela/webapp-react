050d46de0e6b122b39bf776374e0b1ca4b619529
akthernahela <nahela00@gmail.com>
2025-12-15 19:35:19 +0100

    Do an ajax call

--- components/MovieDetail.jsx
+++ components/MovieDetail.jsx
@@ -4,6 +4,16 @@ import axios from "axios";
 
 function MovieDetail() {
 
+    const { id } = useParams();
+    const [movies, setMovies] = useState([]);
+    const urlMovies = 'http://localhost:3000/api/movies/';
+    useEffect(() => {
+        axios.get(urlMovies).then(res => {
+            console.log(res.data);
+            setMovies(res.data);
+        });
+    }, [id]);
+
     return (
         <div className="container">
             <h1>titolo</h1>
