cc43cf2fe5b1fd4e4a804b89b9c79ddbba3a4923
akthernahela <nahela00@gmail.com>
2025-12-15 23:33:20 +0100

    Destructure elements

--- components/NewReview.jsx
+++ components/NewReview.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 
 
-export default function NewReview() {
+export default function NewReview({ movieId, onReviewAdded }) {
 
     return
 }
