156ff747dc8532fe71ca839958f188cdc80e56d5
akthernahela <nahela00@gmail.com>
2025-12-16 11:42:17 +0100

    Now react know what i write inside the name

--- components/NewReview.jsx
+++ components/NewReview.jsx
@@ -16,7 +16,7 @@ export default function NewReview({ movieId, onReviewAdded }) {
         <>
             <form onSubmit={handleSubmit}>
                 <div className="row mb-3">
-                    <label htmlFor="inputName" className="col-sm-2 col-form-label" value={name}>Nome</label>
+                    <label htmlFor="inputName" className="col-sm-2 col-form-label" value={name} onChange={(event) => setName(event.target.value)}>Nome</label>
                     <div className="col-sm-10">
                         <input type="text" className="form-control" id="inputName" />
                     </div>
