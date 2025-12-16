7bd6f2a729c43d5fe7c714c0be12f7d3746b2b37
akthernahela <nahela00@gmail.com>
2025-12-16 11:51:15 +0100

    React don't use selected

--- components/NewReview.jsx
+++ components/NewReview.jsx
@@ -29,12 +29,11 @@ export default function NewReview({ movieId, onReviewAdded }) {
                 </div>
                 <div className="row mb-3">
                     <select className="form-select form-select-sm" value={vote} onChange={(event) => setVote(Number(event.target.value))} aria-label="Small select example">
-                        <option selected>Vote</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                        <option value="4">Four</option>
-                        <option value="5">Five</option>
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
                     </select>
                 </div>
                 <div className="row mb-3">
