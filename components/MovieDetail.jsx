11aac729ff6bc2d3167417b812643a6207d4a21d
akthernahela <nahela00@gmail.com>
2025-12-16 13:00:04 +0100

    Import the new review page

--- components/MovieDetail.jsx
+++ components/MovieDetail.jsx
@@ -1,6 +1,8 @@
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import NewReview from "./NewReview";
+
 
 function MovieDetail() {
 
