e0bb1d25c794a45a0fd5efdfe15adbfb628162bd
akthernahela <nahela00@gmail.com>
2025-12-16 10:48:43 +0100

    Add the text area

--- components/NewReview.jsx
+++ components/NewReview.jsx
@@ -16,16 +16,14 @@ export default function NewReview({ movieId, onReviewAdded }) {
         <>
             <form>
                 <div className="row mb-3">
-                    <label for="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+                    <label for="inputEmail3" className="col-sm-2 col-form-label">Nome</label>
                     <div className="col-sm-10">
                         <input type="email" className="form-control" id="inputEmail3" />
                     </div>
                 </div>
                 <div className="row mb-3">
-                    <label for="inputPassword3" className="col-sm-2 col-form-label">Password</label>
-                    <div className="col-sm-10">
-                        <input type="password" className="form-control" id="inputPassword3" />
-                    </div>
+                    <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
                 <button type="submit" className="btn btn-primary">Sign in</button>
             </form>
