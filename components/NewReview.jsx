38ef0cbbad4b670a37538b66b04a41f222c10426
akthernahela <nahela00@gmail.com>
2025-12-16 11:28:21 +0100

    Fix the input

--- components/NewReview.jsx
+++ components/NewReview.jsx
@@ -16,16 +16,16 @@ export default function NewReview({ movieId, onReviewAdded }) {
         <>
             <form>
                 <div className="row mb-3">
-                    <label for="inputEmail3" className="col-sm-2 col-form-label">Nome</label>
+                    <label for="inputName" className="col-sm-2 col-form-label">Nome</label>
                     <div className="col-sm-10">
-                        <input type="email" className="form-control" id="inputEmail3" />
+                        <input type="text" className="form-control" id="inputName" />
                     </div>
                 </div>
                 <div className="row mb-3">
-                    <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <label for="exampleFormControlTextarea1" className="form-label">Scrivi la tua esperienza</label>
                     <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
-                <button type="submit" className="btn btn-primary">Sign in</button>
+                <button type="submit" className="btn btn-primary">Pubblica</button>
             </form>
         </>
     )
